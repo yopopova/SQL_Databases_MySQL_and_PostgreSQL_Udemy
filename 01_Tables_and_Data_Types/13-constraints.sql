557927f9c2378fd65a45ab16f2aefa5ac21512be
Yoanna Popova <yoanna.dp@gmail.com>
2025-05-07 16:12:36 +0300

    Set up users table columns.

---
+++ 01_Tables_and_Data_Types/13-constraints.sql
@@ -0,0 +1,7 @@
+-- ALTER TABLE users
+-- ALTER COLUMN full_name SET NOT NULL,
+-- ALTER COLUMN current_status SET NOT NULL; -- PostgreSQL
+
+ALTER TABLE users
+MODIFY COLUMN full_name VARCHAR(300) NOT NULL,
+MODIFY COLUMN current_status ENUM('employed', 'self-employed', 'unemployed') NOT NULL; -- MySQL
