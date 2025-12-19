6fdaa36d05b458f129e29a93c4d8ea026127c649
Yoanna Popova <yoanna.dp@gmail.com>
2025-12-19 12:40:37 +0200

    Make two new tables for employees and intranet_accounts.

---
+++ 03_Data_Normalization_and_Relationships/17-relationships.sql
@@ -0,0 +1,16 @@
+CREATE TABLE employees (
+  --id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+  id SERIAL PRIMARY KEY, -- PostgreSQL
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  birthdate DATE NOT NULL,
+  -- email VARCHAR(200) REFERENCES intranet_accounts ON DELETE
+  email VARCHAR(200) UNIQUE NOT NULL
+);
+
+CREATE TABLE intranet_accounts (
+  --id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+  id SERIAL PRIMARY KEY, -- PostgreSQL
+  email VARCHAR(200) REFERENCES employees (email) ON DELETE CASCADE,
+  password VARCHAR(200) NOT NULL
+);
